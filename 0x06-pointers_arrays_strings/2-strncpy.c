b896062314086b5dce5ed91f7e644eb3f2831a7f
ETG1 <elijahkazungu22@gmail.com>
2023-03-02 17:56:49 -0800

    2-strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,12 +1,10 @@
 #include "main.h"
 /**
 * _strncpy - copy a string
-* @dest: input value
-* @src: input value
-* @n: input value
 *
 * Return: dest
 */
+
 char *_strncpy(char *dest, char *src, int n)
 {
 int j;
@@ -21,4 +19,5 @@ while (j < n)
 dest[j] = '\0';
 j++;
 }
-ret
+return (dest);
+}
