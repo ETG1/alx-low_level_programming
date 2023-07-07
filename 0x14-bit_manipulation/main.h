f7c30dab672a8c9d00c250428065c22e3568fb57
ETG1 <elijahkazungu22@gmail.com>
2023-07-07 07:42:48 -0700

    update

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -4,7 +4,7 @@
 unsigned int binary_to_uint(const char *d);
 void print_binary(unsigned long int e);
 int get_bit(unsigned long int n, unsigned int index);
-int _putchar(char c );
+int _putchar(char c);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *e, unsigned int index);
 int get_endianness(void);
