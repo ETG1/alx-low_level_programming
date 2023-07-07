a2becf352cd1d2c08193b267619df027c6718255
ETG1 <elijahkazungu22@gmail.com>
2023-07-06 18:05:16 -0700

    header file

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+unsigned int binary_to_uint(const char *d);
+void print_binary(unsigned long int e);
+int get_bit(unsigned long int n, unsigned int index);
+int _putchar(char c );
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *e, unsigned int index);
+int get_endianness(void);
+unsigned int flip_bits(unsigned long int a, unsigned long int b);
+
+#endif
+
