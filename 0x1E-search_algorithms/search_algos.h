5b67219aa3dc896159d9c4a7d01ff3cfb8468feb
ETG1 <elijahkazungu22@gmail.com>
2024-03-13 20:17:11 -0700

    Header

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -52,4 +52,5 @@ int exponential_search(int *array, size_t size, int value);
 size_t min(size_t a, size_t b);
 int interpolation_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
+
 #endif
